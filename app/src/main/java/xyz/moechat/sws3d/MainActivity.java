dc5a14f3410e61e6c7de0144ae00fdae77ab51ba
timeloveboy <lipeng@mirrorsoft.cn>
2016-05-01 08:19:38 +0800

    ~

--- app/src/main/java/xyz/moechat/sws3d/MainActivity.java
+++ app/src/main/java/xyz/moechat/sws3d/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Matrix;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -25,11 +26,13 @@ public class MainActivity extends Activity  implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         sws3dView=(Sws3dView)findViewById(R.id.textView);
 
-        sws3dView.setRotate_X(104);sws3dView.setRotate_Y(0);sws3dView.setRotate_Z(0);
-        sws3dView.setTranslate_X(-600);
+        sws3dView.setRotate_X(100);sws3dView.setRotate_Y(0);sws3dView.setRotate_Z(0);
+
         sws3dView.setTranslate_Y(800);
         sws3dView.setTranslate_Z(400);
 
+        sws3dView.setLocation_x(6);
+        sws3dView.setTranslate_X(0);
         Button b_1=(Button)findViewById(R.id.b_1);
         Button b_2=(Button)findViewById(R.id.b_2);
         Button b_3=(Button)findViewById(R.id.b_3);
@@ -43,6 +46,14 @@ public class MainActivity extends Activity  implements View.OnClickListener {
         Button b5=(Button)findViewById(R.id.b5);
         Button b6=(Button)findViewById(R.id.b6);
 
+        Button button1=(Button)findViewById(R.id.button1);
+        Button button2=(Button)findViewById(R.id.button2);
+        Button button3=(Button)findViewById(R.id.button3);
+        Button button4=(Button)findViewById(R.id.button4);
+        Button button5=(Button)findViewById(R.id.button5);
+        Button button6=(Button)findViewById(R.id.button6);
+
+
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
@@ -55,6 +66,12 @@ public class MainActivity extends Activity  implements View.OnClickListener {
         b_4.setOnClickListener(this);
         b_5.setOnClickListener(this);
         b_6.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
 
         Intent intent = new Intent("xyz.moechat.sw3d.MUSIC");
         intent.setPackage(getPackageName());
@@ -92,13 +109,13 @@ public class MainActivity extends Activity  implements View.OnClickListener {
                 break;
 
             case R.id.b_1:
-                sws3dView.setRotate_X(sws3dView.getRotate_X()-1);
+                sws3dView.setRotate_X(sws3dView.getRotate_X() - 1);
                 break;
             case R.id.b_2:
-                sws3dView.setRotate_Y(sws3dView.getRotate_Y() -1);
+                sws3dView.setRotate_Y(sws3dView.getRotate_Y() - 1);
                 break;
             case R.id.b_3:
-                sws3dView.setRotate_Z(sws3dView.getRotate_Z()- 1);
+                sws3dView.setRotate_Z(sws3dView.getRotate_Z() - 1);
                 break;
             case R.id.b_4:
                 sws3dView.setTranslate_X(sws3dView.getTranslate_X() - 100);
@@ -109,9 +126,31 @@ public class MainActivity extends Activity  implements View.OnClickListener {
             case R.id.b_6:
                 sws3dView.setTranslate_Z(sws3dView.getTranslate_Z() - 100);
                 break;
-        }
 
-        Log.v("moe","rotate:("+sws3dView.getRotate_X()+","+sws3dView.getRotate_Y()+","+sws3dView.getRotate_Z()+")"+"translate:("+sws3dView.getTranslate_X()+","+sws3dView.getTranslate_Y()+","+sws3dView.getTranslate_Z()+")");
+            case R.id.button1:
+                sws3dView.setLocation_x(sws3dView.getLocation_x() - 1);
+                break;
+            case R.id.button2:
+                sws3dView.setLocation_y(sws3dView.getLocation_y() - 1);
+                break;
+            case R.id.button3:
+                sws3dView.setLocation_z(sws3dView.getLocation_z() - 1);
+                break;
+            case R.id.button4:
+                sws3dView.setLocation_x(sws3dView.getLocation_x() +1);
+                break;
+            case R.id.button5:
+                sws3dView.setLocation_y(sws3dView.getLocation_y() +1);
+                break;
+            case R.id.button6:
+                sws3dView.setLocation_z(sws3dView.getLocation_z() +1);
+                break;
+        }
+        Matrix matrix=new Matrix();
+        sws3dView.getCamera().getMatrix(matrix);
+        Log.v("moe",matrix.toShortString());
+        Log.v("moe","/:"+sws3dView.translate_X/sws3dView.location_x);
+        Log.v("moe","loaction:"+sws3dView.location_x+","+sws3dView.location_y+","+sws3dView.location_z+"rotate:("+sws3dView.getRotate_X()+","+sws3dView.getRotate_Y()+","+sws3dView.getRotate_Z()+")"+"translate:("+sws3dView.getTranslate_X()+","+sws3dView.getTranslate_Y()+","+sws3dView.getTranslate_Z()+")");
     }
 
     @Override
@@ -128,7 +167,7 @@ public class MainActivity extends Activity  implements View.OnClickListener {
                         e.printStackTrace();
                     }
                     Message m=new Message();
-                    //// TODO: 16/5/1  
+                    //// TODO: 16/5/1
                     m.what=1;
                     handler.sendMessage(m);
                 }
@@ -153,7 +192,9 @@ public class MainActivity extends Activity  implements View.OnClickListener {
                 sws3dView.setText(sws3dView.getText() + getMixString(20) + split_tag);
                 sws3dView.startScroll();
             }
+            //Log.v("moe",sws3dView.getHeight()+"");
         }
+
     };
 
     //获取固定长度的随机字符串
--- app/src/main/java/xyz/moechat/sws3d/Sws3dView.java
+++ app/src/main/java/xyz/moechat/sws3d/Sws3dView.java
@@ -1,20 +1,19 @@
 package xyz.moechat.sws3d;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.text.TextUtils;
+import android.renderscript.Matrix4f;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.animation.LinearInterpolator;
-import android.widget.ScrollView;
 import android.widget.Scroller;
 import android.widget.TextView;
-
-import java.util.List;
 
 
 /**
@@ -24,6 +23,10 @@ public class Sws3dView extends TextView {
     public Sws3dView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         setWillNotDraw(false);
+        mTextPaint = new TextPaint();
+
+        // Set up a default TextPaint object
+        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
     }
 
     public int getRotate_X() {
@@ -51,8 +54,6 @@ public class Sws3dView extends TextView {
     }
 
     int rotate_X=80,rotate_Y=0,rotate_Z=0;
-    float translate_X=0;
-    float translate_Y=1000f;
 
     public float getTranslate_Z() {
         return translate_Z;
@@ -78,19 +79,107 @@ public class Sws3dView extends TextView {
         this.translate_Y = translate_Y;
     }
 
+    float translate_X=0;
+    float translate_Y=1000f;
     float translate_Z=1000f;
-    protected void onDraw(Canvas canvas) {
-        Camera c = new Camera();
-        c.save();
-        c.rotate(rotate_X, rotate_Y, rotate_Z);
-        c.translate(translate_X, translate_Y, translate_Z);
 
-        c.applyToCanvas(canvas);
-        c.restore();
-        super.onDraw(canvas);
+    public float getLocation_x() {
+        return location_x;
     }
 
-    //// TODO: 16/5/1  
+    public void setLocation_x(float location_x) {
+        this.location_x = location_x;
+    }
+
+    public float getLocation_y() {
+        return location_y;
+    }
+
+    public void setLocation_y(float location_y) {
+        this.location_y = location_y;
+    }
+
+    public float getLocation_z() {
+        return location_z;
+    }
+
+    public void setLocation_z(float location_z) {
+        this.location_z = location_z;
+    }
+
+    float location_x=0, location_y=0, location_z=-8;
+    public Camera getCamera() {
+        return camera;
+    }
+    private Matrix mMatrix = new Matrix();
+    Camera camera= new Camera();;
+    private float mAngle = 60f;
+    private float mScrollPosition = 0f;
+    private float mEndScrollMult = 2f;
+    private float mDistanceFromText = 0f;
+
+    private TextPaint mTextPaint;
+    private StaticLayout mTextLayout;
+    protected void onDraw(Canvas canvas) {
+
+//        int contentWidth = getWidth()  ;
+//        int contentHeight = getHeight()  ;
+//
+//        final int saveCnt = canvas.save();
+//
+//        // Rotate/translate the camera
+//        canvas.getMatrix(mMatrix);
+//        camera.save();
+//
+//        int cX = contentWidth / 2  ;
+//        int cY = contentHeight / 2 ;
+//        camera.rotateX(mAngle);
+//        camera.translate(0, 0, mDistanceFromText);
+//        camera.getMatrix(mMatrix);
+//        mMatrix.preTranslate(-cX, -cY);
+//        mMatrix.postTranslate(cX, cY);
+//        camera.restore();
+//
+//        canvas.concat(mMatrix);
+//
+//        // The end scroll multiplier ensures that the text scrolls completely out of view
+//        canvas.translate(0f, contentHeight - mScrollPosition *
+//                (getHeight() + mEndScrollMult * contentHeight));
+//
+//        // Draw the text
+//        mTextLayout.draw(canvas);
+//
+//        canvas.restoreToCount(saveCnt);
+
+
+        camera.save();
+        camera.setLocation(location_x, location_y, location_z);
+
+        camera.rotate(rotate_X, rotate_Y, rotate_Z);
+        camera.translate(translate_X, translate_Y, translate_Z);
+
+        Matrix4f matrix4f=new Matrix4f();
+        camera.applyToCanvas(canvas);
+        camera.restore();
+        super.onDraw(canvas);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        // Measure and layout the text
+        final CharSequence text = getText();
+        measureAndLayoutText(text);
+    }
+
+    private void measureAndLayoutText(CharSequence text) {
+
+
+        int availableWidth = getWidth()  ;
+        mTextLayout = new StaticLayout(text, mTextPaint, availableWidth, Layout.Alignment.ALIGN_CENTER,
+                1.1f, 0f, true);
+    }
+    //// TODO: 16/5/1
     //region 滚动
     int Duration=50;
 
@@ -142,5 +231,6 @@ public class Sws3dView extends TextView {
         mPaused = false;
     }
     //endregion
+
 }
 
